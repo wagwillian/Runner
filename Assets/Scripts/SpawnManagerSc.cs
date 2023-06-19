373d7ec075874ba12499df74b59aef7809a2f23e
Wagner Willian <wwop09@gmail.com>
2023-06-19 14:49:49 -0300

    Added Obstacles and movement to the scene

---
+++ Assets/Scripts/SpawnManagerSc.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerSc : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPosition = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPosition, obstaclePrefab.transform.rotation);
+    }
+}
